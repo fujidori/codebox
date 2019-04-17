81c58138b1cc2edd579a62d09876fa734c44f049
fujidori <fujidori9@gmail.com>
2019-04-17 11:08:50 +0900

    Add crackme0x00

---
+++ crackme0x00/sections.py
@@ -0,0 +1,7 @@
+from elftools.elf.elffile import ELFFile
+
+with open('./chall.elf', 'rb') as f:
+    e = ELFFile(f)
+    for section in e.iter_sections():
+        print(hex(section['sh_addr']), section.name)
+
